59597b60bd1c1ac5c4ed0db5c82264041ccc41c3
Kean <keandheb.baba@lsu.edu.ph>
2025-09-23 09:24:26 +0800

    input field dev

--- Animoverse/src/_auth/forms/SignupForm.tsx
+++ Animoverse/src/_auth/forms/SignupForm.tsx
@@ -42,7 +42,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
 
-      <div className="sm:w-420 flex-center flex flex-col">
+      <div className="flex flex-col items-center">
         <img src="/assets/images/logo.png" />
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">To use Animoverse, enter your account details</p>
@@ -50,12 +50,12 @@ const SignupForm = () => {
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
           <FormField
             control={form.control}
-            name="username"
+            name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Name</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input type="text" className="shad-input"{...field} />
                 </FormControl>
                 <FormDescription>
                   This is your public display name.
